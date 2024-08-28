461a6558a6737829fac8d343dd3aef7bd995a179
Grunt395 <grantlew@att.net>
2024-08-28 16:18:34 -0700

    Added media queries

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 
 let secretNumber = Math.floor(Math.random() * 20) + 1;
-let guess = 0;
 let score = 20;
 let highScore = 0;
 
+// Function for changing displayed message
+function displayText(text) {
+    document.querySelector(".text").textContent = text;
+}
+
 document.querySelector(".check").addEventListener("click", function() {
-    guess = Number(document.querySelector(".guess").value);
+    let guess = Number(document.querySelector(".guess").value);
     
+    // Empty Guess
     if (!guess) {
-        document.querySelector(".text").textContent = "No guess...";
+        displayText("No guess...");
     }
     
+    // Correct Guess
     else if (guess === secretNumber) {
         if (score > highScore) {
             highScore = score;
         }
-        document.querySelector(".text").textContent = "You Win!";
+        displayText("You Win!");
         document.querySelector(".number").textContent = secretNumber;
         document.querySelector(".highscore").textContent = highScore;
         document.querySelector(".number").style.width = "15rem";
         document.querySelector("body").style.backgroundColor = "#60b347";
     }
     
+    // Too High or Low
     else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector(".text").textContent = guess > secretNumber ? "Too High!" : "Too Low!";
+            displayText(guess > secretNumber ? "Too High!" : "Too Low!");
             score--;
             document.querySelector(".score").textContent = score;
         }
@@ -32,10 +39,11 @@ document.querySelector(".check").addEventListener("click", function() {
 
 });
 
+// Reset Button
 document.querySelector(".again").addEventListener("click", function() {
     score = 20;
     secretNumber = Math.floor(Math.random() * 20) + 1;
-    document.querySelector(".text").textContent = "Start Guessing...";
+    displayText("Start Guessing...");
     document.querySelector(".number").textContent = "?";
     document.querySelector(".score").textContent = score;
     document.querySelector("body").style.backgroundColor = "#222";
